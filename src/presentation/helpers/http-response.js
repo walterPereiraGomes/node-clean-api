70997a367bd65efe1b710c07a87379ee5c60f32b
Walter <walterpereira@deltaglobal.com.br>
2021-12-25 10:42:08 -0300

    refactor to separated files

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,16 @@
+const MissingParamError = require('./missing-param-error')
+
+module.exports = class HttpResponse {
+  static badRequest (message) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(message)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
