7e5b2424d9d0a1d2ee0b660f3293881484b83a0f
Walter Pereira <walterpereira@deltainova.com.br>
2021-12-29 12:29:10 -0300

    move EmailValidator to its own file

--- __mocks__/validator.js
+++ __mocks__/validator.js
@@ -1,7 +1,8 @@
 module.exports = {
   isEmailValid: true,
-
+  email: null,
   isEmail (email) {
+    this.email = email
     return this.isEmailValid
   }
 }
--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,24 +1,25 @@
+const EmailValidator = require('./email-validator')
 const validator = require('validator')
 
-class EmailValidator {
-  isValid (email) {
-    return validator.isEmail(email)
-  }
+const makeSut = () => {
+  return new EmailValidator()
 }
 
 describe('Email Validator', () => {
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@email.com')
     expect(isEmailValid).toBe(true)
   })
-})
-
-describe('Email Validator', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_email')
     expect(isEmailValid).toBe(false)
+  })
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@email.com')
+    expect(validator.email).toBe('any_email@email.com')
   })
 })
