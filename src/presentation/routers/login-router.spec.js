4f105a4ec757308a80d1637fac7ff0faa05ea5cd
Walter Pereira <walterpereira@deltainova.com.br>
2021-12-29 11:51:30 -0300

    refactor errors

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,5 @@
 const LoginRouter = require('./login-router')
-const MissingParamError = require('../helpers/missing-param-error')
-const UnauthorizedError = require('../helpers/unauthorized-error')
-const ServerError = require('../helpers/server-error')
-const InvalidParamError = require('../helpers/invalid-param-error')
+const { InvalidParamError, ServerError, MissingParamError, UnauthorizedError } = require('../errors')
 
 const timeout = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms))
